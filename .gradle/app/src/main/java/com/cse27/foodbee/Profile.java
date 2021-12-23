01682117c064dc3ba4d5871f32a531a43c924e2c
nishat5349 <tasnim.nishat5349@gmail.com>
2021-12-23 14:14:42 +0600

    added profile page and update profile page

---
+++ .gradle/app/src/main/java/com/cse27/foodbee/Profile.java
@@ -0,0 +1,31 @@
+package com.cse27.foodbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.QuickContactBadge;
+
+public class Profile extends AppCompatActivity {
+    private Button btnUpdateProfile;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        btnUpdateProfile = findViewById(R.id.btnUpdateProfile);
+        btnUpdateProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToUpdateProfile();
+            }
+
+        });
+    }
+    public void goToUpdateProfile(){
+         Intent intent= new Intent(this, UpdateProfile.class );
+         startActivity(intent);
+    }
+}
