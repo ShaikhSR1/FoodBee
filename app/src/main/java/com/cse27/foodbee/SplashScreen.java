bf9d5bf9af9687369e4e5625b919b723466fedd5
ShaikhSR1 <shaikhsr.cse@gmail.com>
2021-12-23 13:59:06 +0600

    Search Box Page Added

---
+++ app/src/main/java/com/cse27/foodbee/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.cse27.foodbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    // Splash Screen Timer
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler = new Handler();
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+
+                // close this activity
+                finish();
+            }
+        }, 5000);
+    }
+}
