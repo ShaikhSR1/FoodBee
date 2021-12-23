f0c1b2de36c6df8a2dffd99fcf02c3df46c038a3
ShaikhSR1 <shaikhsr.cse@gmail.com>
2021-12-23 13:48:56 +0600

    Splash Screen Added and Login file updated

--- app/src/main/java/com/cse27/foodbee/SignUp.java
+++ app/src/main/java/com/cse27/foodbee/SignUp.java
@@ -3,9 +3,10 @@ package com.cse27.foodbee;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
-
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
