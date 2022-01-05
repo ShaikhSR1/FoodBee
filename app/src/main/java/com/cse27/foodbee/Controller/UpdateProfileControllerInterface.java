4aa6e0f4cc41011be1d7e2fd7530ff9396e2361c
nishat5349 <tasnim.nishat5349@gmail.com>
2022-01-05 17:53:27 +0600

    Basic work with MVC is done

---
+++ app/src/main/java/com/cse27/foodbee/Controller/UpdateProfileControllerInterface.java
@@ -0,0 +1,5 @@
+package com.cse27.foodbee.Controller;
+
+public interface UpdateProfileControllerInterface {
+    void onUpdateProfile(String fullName,String email,String phoneNumber, String address ,String password);
+}
