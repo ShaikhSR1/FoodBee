6bd4a7a61604b845b763abdd2256869bb13a429c
ShaikhSR1 <shaikhsr.cse@gmail.com>
2022-01-08 00:11:22 +0600

    Home page icons added

--- app/src/main/java/com/cse27/foodbee/Fragments/HomeFragment.java
+++ app/src/main/java/com/cse27/foodbee/Fragments/HomeFragment.java
@@ -1,16 +1,24 @@
 package com.cse27.foodbee.Fragments;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
+import com.cse27.foodbee.CartPage;
+import com.cse27.foodbee.Profile;
 import com.cse27.foodbee.R;
+
+import java.util.Objects;
 
 public class HomeFragment extends Fragment {
 
+    ImageView imageViewCart, imageViewProfile;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -22,7 +30,26 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_home, container, false);
 
+        imageViewCart = root.findViewById(R.id.imageViewCart);
+        imageViewProfile = root.findViewById(R.id.imageViewProfile);
 
+        imageViewProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Profile.class);
+                startActivity(intent);
+                ((Activity) requireActivity()).overridePendingTransition(0, 0);
+            }
+        });
+
+        imageViewCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CartPage.class);
+                startActivity(intent);
+                ((Activity) getActivity()).overridePendingTransition(0, 0);
+            }
+        });
 
         return root;
     }
