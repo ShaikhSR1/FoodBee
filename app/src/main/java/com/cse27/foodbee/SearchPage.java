4fe51264da1b8d07f757a8611493b5185fda0f9c
ShaikhSR1 <shaikhsr.cse@gmail.com>
2022-01-08 01:25:11 +0600

    Showing result from firebase

--- app/src/main/java/com/cse27/foodbee/SearchPage.java
+++ app/src/main/java/com/cse27/foodbee/SearchPage.java
@@ -1,14 +1,60 @@
 package com.cse27.foodbee;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class SearchPage extends AppCompatActivity {
 
+    SearchResult searchResult;
+    FirebaseFirestore firestoreSearch = FirebaseFirestore.getInstance();
+    Button btnSearchFood;
+    EditText editTextSearch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_page);
+
+        btnSearchFood = findViewById(R.id.btnSearchFood);
+        editTextSearch = findViewById(R.id.editTextSearch);
+
+        btnSearchFood.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firebaseUserSearch();
+            }
+        });
     }
+
+    private void firebaseUserSearch() {
+        firestoreSearch.collection("foods")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                    @Override
+                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                        for (QueryDocumentSnapshot documents : queryDocumentSnapshots) {
+                            if(documents.getString("type")==editTextSearch.toString()) {
+                                searchResult.showResults(documents.getString("imageurl"), documents.getString("name"), documents.getString("rating"), documents.getString("price"));
+                            }
+                        }
+                    }
+                });
+    }
+
+    /*public class SearchResultFromDB extends RecyclerView.ViewHolder {
+        View view;
+        public SearchResultFromDB(View itemView) {
+            super(itemView);
+            view = itemView;
+        }
+    }*/
 }
---
+++ app/src/main/java/com/cse27/foodbee/SearchResult.java
@@ -0,0 +1,52 @@
+package com.cse27.foodbee;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.cse27.foodbee.Model.FoodModel;
+import com.cse27.foodbee.Recycler.FoodRecyclerAdapter;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.squareup.picasso.Picasso;
+
+public class SearchResult extends AppCompatActivity {
+
+    Context context;
+    FirebaseFirestore firestoreSearch = FirebaseFirestore.getInstance();
+    FoodRecyclerAdapter foodRecyclerAdapter;
+
+    String foodImageUrl, foodName, foodRating, foodPrice;
+
+    ImageView imageFoodCard;
+    TextView textViewFoodNameCard, textViewFoodRatingCard, textViewFoodPriceCard;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_result);
+
+        imageFoodCard = findViewById(R.id.imageFoodCard);
+        textViewFoodNameCard = findViewById(R.id.textViewFoodNameCard);
+        textViewFoodRatingCard = findViewById(R.id.textViewFoodRatingCard);
+        textViewFoodPriceCard = findViewById(R.id.textViewFoodPriceCard);
+        
+
+
+    }
+
+
+    public void showResults(String url, String name, String rating, String price) {
+
+        Picasso.with(context).load(url).fit().into(imageFoodCard);
+        textViewFoodNameCard.setText(name);
+        textViewFoodRatingCard.setText(rating);
+        textViewFoodPriceCard.setText(price);
+    }
+}
