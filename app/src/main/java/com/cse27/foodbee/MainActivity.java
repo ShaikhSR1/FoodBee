71e9cfdd2e8b59c2f250a8a1a73e4735a8fed97e
Nishat Tasnim <72455268+Nishat5349@users.noreply.github.com>
2022-01-09 21:28:13 +0600

    Update MainActivity.java

--- app/src/main/java/com/cse27/foodbee/MainActivity.java
+++ app/src/main/java/com/cse27/foodbee/MainActivity.java
@@ -4,6 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+/**
+ * Application
+ * @author :
+ * <br>
+ * Asraf Ullah Rahat <br>
+ * Nishat Tasnim <br>
+ * Abdullah Al Shaik <br>
+ * Jubair Al Faisal <br>
+ * Jannatul Ferdaus Amrin <br>
+ * @version 1.0
+ * @since 02 january, 2021
+ */
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
