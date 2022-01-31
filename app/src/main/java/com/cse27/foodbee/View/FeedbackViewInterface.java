08d3bb82435ae87acfefc1f55bf86ca13a0eb947
Jannatul Ferdaws Amrin <45651547+jannatulferdaws@users.noreply.github.com>
2022-01-31 21:03:10 +0600

    Update FeedbackViewInterface.java

--- app/src/main/java/com/cse27/foodbee/View/FeedbackViewInterface.java
+++ app/src/main/java/com/cse27/foodbee/View/FeedbackViewInterface.java
@@ -3,6 +3,30 @@ package com.cse27.foodbee.View;
 
 import android.widget.RatingBar;
 
+/**
+ * this feedabackViewInterface contains the onRatingChanged function initialization
+ */
 public interface FeedbackViewInterface {
+    /**
+     * On rating changed.
+     *
+     * @param ratingBar the rating bar
+     * @param rating    the rating
+     * @param fromUser  the from user
+     */
     void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser);
+
+    /**
+     * On feedback success.
+     *
+     * @param message the message
+     */
+    void onFeedbackSuccess(String message);
+
+    /**
+     * On feedback error.
+     *
+     * @param message the message
+     */
+    void onFeedbackError(String message);
 }
